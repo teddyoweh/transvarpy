653b1933da1b332e1d5bce3e7aa9231c8b52ddd9
Ifechukwudeni Oweh <76883580+teddyoweh@users.noreply.github.com>
2022-04-09 16:07:55 -0500

    update

--- code.py
+++ code.py
@@ -53,11 +53,30 @@ class TransDict:
             
         
 class TransIndv:       
-   def __init__(self,item,value):
+    def __init__(self,item,value):
         """ app"""
         self.item = item
         
         self.globals = globals()[item]= value
+        
+      
+    def getdictval_value(self,string:str):
+      
+        return [t for x, t in enumerate(self.items) if t==string][0]
+    def getdictval_index(self,index:int):
+       
+        return [t for x, t in enumerate(self.items) if x==int(index)][0]
+    def add_prefix(self,prefix_a):
+         
+        pref = f'{self.item}{prefix_a}'
+        self.globals = globals()[pref]= self.item
+    def add_suffix(self,suffix_a):
+      
+        suff = f'{suffix_a}{self.item}'
+        self.varname = suff
+        self.globals = globals()[suff]= self.items
+          
+
 
 TransIndv('x','boyyy')
 
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from code import TransIndv
-import code
-from code import *
- 
- 
-print(name)
+def gettype(input):
+    return str(type(input)).replace("<class '",'').replace("'>",'')
+
+
+oo  = {'sd':4}
+print(gettype(oo))
+
+
+types  = ['str','list','dict','float','int','long']
