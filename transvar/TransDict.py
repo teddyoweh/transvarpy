97fa5b82385a2fb49907ef00671d226e86b03555
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-30 00:54:34 -0500

    Update TransDict.py

--- transvar/TransDict.py
+++ transvar/TransDict.py
@@ -19,7 +19,7 @@ class transDict:
         
     def add_suffix_all(self,suffix_a):
         """
-         Add a suffix to all the variables that has been created
+         Add a suffx to all the variables that has been created
         
         """
         for s in self.items:
