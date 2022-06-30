5bcdd19d6a5fa4029525f513dcfc90473fffdd2f
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-30 05:31:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup
 
 setup(
-   name='transvar',
+   name='transvarpy',
    version='0.1.0',
    author='teddyoweh',
    author_email='teddy@teddyoweh.net',
-   packages=['package_name', 'package_name.test'],
-   scripts=['transvar/__init__.py'],
-   url='http://pypi.python.org/pypi/transvar/',
+   packages=['transvarpy'],
+   scripts=['transvarpy/__init__.py'],
    license='LICENSE.txt',
+   maintainer_email='teddyoweh@teddyoweh.net',
+   maintainer='teddyoweh',
+   url='https://github.com/teddyoweh/transvarpy/',
+   
    description='A new method of declaring and editing variable names',
    long_description=open('README.md').read(),
-    keywords=[ 'transvar','python transvar','python', 'python variables','variables'],
+   long_description_content_type='text/markdown',
+    keywords=[ 'transvarpuy','python transvar','python', 'python variables','variables'],
     classifiers= [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
