0875381c998c57a2d29ed131ae3d0a50bc9ca8ff
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-30 01:02:40 -0500

    Update TransDict.py

--- transvar/TransDict.py
+++ transvar/TransDict.py
@@ -6,7 +6,7 @@ class transDict:
             self.globals = globals()[i]= items[i]
     def init(self,_globals):
         """
-        taha been created.
+        taa been created.
     
         """
         return(_globals.update(globals()))
