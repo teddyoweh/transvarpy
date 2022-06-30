1e8c322ad4989581456dfccec68ef3dd4d2d641b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-30 01:01:05 -0500

    Update TransDict.py

--- transvar/TransDict.py
+++ transvar/TransDict.py
@@ -6,7 +6,7 @@ class transDict:
             self.globals = globals()[i]= items[i]
     def init(self,_globals):
         """
-            to all the variables that has been created.
+            all the variables that has been created.
         
         """
         return(_globals.update(globals()))
